82435ccc45e4680eebe14fe39a03b8d5cbd91ba0
davidtan <davidtan@thoughtworks.com>
2018-01-10 15:22:18 +0800

    Create assignment

--- test/kebabize.test.js
+++ test/kebabize.test.js
@@ -1,10 +1,10 @@
 const kebabize = require('../src/kebabize')
 
-test('dummy', () => {
+test('hiThere should be kebabized to hi-there', () => {
     expect(kebabize('hiThere')).toBe('hi-there')
 })
 
-test.skip('dummy', () => {
+test.skip('hiThereBruceWayne shuld be kebabized to hi-there-bruce-wayne', () => {
     expect(kebabize('hiThereBruceWayne')).toBe('hi-there-bruce-wayne')
 })
 
